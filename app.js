4ce3a7edd737709b6099a32cc701aa1abe1bf753
shimguh <shimguh@naver.com>
2024-02-05 15:13:27 +0900

    app.js

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const router = express.Router();
 
-app.get('/', (req, res) => {
-	// res.send('Hello World!')
-	let myJson = {"Hello": "World"}
-	res.json(myJson)
-})
+// ex) GET /artists/1, GET /artists/1/company/entertainment
 
-app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`)
-})
+router.get('/artistes/:id', function (req,res) {
+	console.log("id는 " + req.params.id + " 입니다")
+	res.send("id : " + req.params.id)
+});
+
+// 여러개도 가능
+router.get('/artists/:id/company/:company', function (req, res) {
+	res.send("id : " + req.params.id + " 회사 : " + req.params.company)
+});
+
