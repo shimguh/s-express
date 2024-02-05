0aa2cbecb1eec19fd25d538ef20128fdab92b788
shimguh <shimguh@naver.com>
2024-02-05 15:40:29 +0900

    npm install s-common-fun

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require('express')
+const myCommon = require('s-common-fun')
 const router = express.Router();
+const port = 3000
 
-// ex) GET /artists/1, GET /artists/1/company/entertainment
+app.get('/', (req, res) => {
+	const a = 1
+	const b = 10
+	let myJson = {"Hello": "World", "ADD": myCommon.add(a, b)}
 
-router.get('/artistes/:id', function (req,res) {
-	console.log("id는 " + req.params.id + " 입니다")
-	res.send("id : " + req.params.id)
-});
+	res.json(myJson)
+})
 
-// 여러개도 가능
-router.get('/artists/:id/company/:company', function (req, res) {
-	res.send("id : " + req.params.id + " 회사 : " + req.params.company)
-});
-
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`)
+})
